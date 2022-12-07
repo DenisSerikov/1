293d5723019645675b665c6ad0d4f17f08b5a845
DenisSerikov <113181052+DenisSerikov@users.noreply.github.com>
2022-12-07 20:15:13 +0300

    Delete Практическая 7 directory

--- Практическая 7/7.1.py
+++
@@ -1,7 +0,0 @@
-m = [1,3,6,45,75]
-print(m)
-print(sum(m))
-r = 1
-for i in m:
-    r = r * i
-print(r)
--- Практическая 7/7.2.py
+++
@@ -1,6 +0,0 @@
-m = list(map(float, input().split()))
-mv = sum(m) / len(m)
-for i in range(len(m)):
-    if m[i] == 0:
-        m[i] = mv
-print(m[i], end=" ")
