74d043297f98fd264195463f7fe65df4f0856fc7
DenisSerikov <113181052+DenisSerikov@users.noreply.github.com>
2022-12-08 19:05:50 +0300

    10 вариант
    10 вариант

---
+++ Практическая 9/9.1.py
@@ -0,0 +1,20 @@
+def fun():
+    M=int(input("кол-во сторок:"))
+    N=int(input("кол-во столбцов:"))
+    A=[]
+    P=[]
+    for i in range(M):
+        B=[]
+        for j in range(N):
+            B.append(int(input()))
+        A.append(B)
+    for i in range(M):
+        for j in range(N):
+            print(A[i][j], end='')
+        print()
+
+    for i in range(M):
+        P = sorted(A[i])
+        if A[i]==P or A[i]==P[::-1]:
+            print(max(A[i]))
+fun()
