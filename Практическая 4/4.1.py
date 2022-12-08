634807c268382de9d5c9ee33b481f712f459d5fd
DenisSerikov <113181052+DenisSerikov@users.noreply.github.com>
2022-12-08 19:00:42 +0300

    Delete Практическая 4 directory

--- Практическая 4/4.1.py
+++
@@ -1,6 +0,0 @@
-a = int(input())
-b = int(input())
-v = a
-while v <= b:
-    print(v)
-    v = v + 1
--- Практическая 4/4.9.py
+++
@@ -1,7 +0,0 @@
-def F(n):
-    if n == 1 or n == 2:
-        return 1
-    else:
-        return F(n - 2) + F(n - 1)
-n = int(input())
-print(F(n + 2) - 1)
