fa8764bd781d768dd87a401019c55b091844fa18
DenisSerikov <113181052+DenisSerikov@users.noreply.github.com>
2023-05-30 15:50:15 +0300

    Main.java

--- HashTable/Main.java
+++ HashTable/Main.java
@@ -3,10 +3,10 @@ package HashTable;
 public class Main {
     public static void main(String[] args) {
         Table<Integer, String> myHash = new HTable();
-        myHash.put(30, "Hello");
+        myHash.put(-30, "Hello");
         myHash.put(4, "bye");
         myHash.put(1, "good");
-        myHash.put(5, "morning");
+        myHash.put(-5, "morning");
         myHash.put(32, "table");
         myHash.put(25, "hash");
         myHash.put(17, "english");
@@ -16,12 +16,12 @@ public class Main {
 
         myHash.put(34, "Hel");
         myHash.put(40, "by");
-        myHash.put(10, "go");
+        myHash.put(-10, "go");
         myHash.put(30, "mor");
-        myHash.put(64, "tab");
+        myHash.put(-64, "tab");
         myHash.put(50, "ha");
         myHash.put(174, "eng");
-        myHash.put(60,"rus");
+        myHash.put(-60,"rus");
         myHash.put(13, "Ja");
         myHash.put(88, "Py");
         myHash.remove(32);
